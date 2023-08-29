c9914718491caa09015625895ac772e5474af973
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-08-29 22:11:47 +0200

    Adapted to also export types.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,29 @@
 import {defineConfig} from "vite";
+import dts from "vite-plugin-dts";
+import commonjs from "vite-plugin-commonjs";
 
 export default defineConfig({
     build: {
         lib: {
             entry:  'lib/randexp.ts',
             name: 'randexp'
+        },
+        rollupOptions:{
+            output: {
+                preserveModules: false,
+                dir: './dist',
+                format: 'cjs',
+            },
         }
     },
     test:{
        include: ['test/**/*.spec.ts'],
-    }
+    },
+    plugins: [
+        dts({
+            include:'lib/randexp.ts',
+            insertTypesEntry: true,
+        }),
+        commonjs(),
+    ],
 });
